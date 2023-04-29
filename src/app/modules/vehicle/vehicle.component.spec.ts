586aa9b8c7e8b2bcc950f7685066853bbeb3a21c
Ricardo Vivas <j.vivast@uniandes.edu.co>
2023-04-29 13:00:43 -0500

    Feature: Add unit test

--- src/app/modules/vehicle/vehicle.component.spec.ts
+++ src/app/modules/vehicle/vehicle.component.spec.ts
@@ -1,14 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { VehicleService } from 'src/app/services/vehicle.service';
+import { faker } from '@faker-js/faker';
 import { VehicleComponent } from './vehicle.component';
+import { Vehicle } from 'src/app/models/vehicle';
+import { By } from '@angular/platform-browser';
 
 describe('VehicleComponent', () => {
   let component: VehicleComponent;
   let fixture: ComponentFixture<VehicleComponent>;
+  let debug: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ VehicleComponent ]
+      declarations: [ VehicleComponent ],
+      imports: [HttpClientModule],
+      providers: [VehicleService]
     })
     .compileComponents();
   });
@@ -16,10 +24,38 @@ describe('VehicleComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehicleComponent);
     component = fixture.componentInstance;
+
+    component.listVehicles = Array.from({length: 3}, () =>{
+      const id = parseInt(faker.random.numeric());
+      const marca = faker.name.firstName(); 
+      const linea = faker.commerce.productName();
+      const referencia = faker.company.name();
+      const modelo = parseInt(faker.random.numeric());
+      const kilometraje = parseInt(faker.random.numeric());
+      const color = faker.color.human();
+      const imagen = faker.image.imageUrl();
+
+      return new Vehicle(id,marca,linea,referencia,modelo,kilometraje,color,imagen);
+    })
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have 3 tr elements', () => {
+    expect(debug.queryAll(By.css('.filas'))).toHaveSize(3);
+  });
+
+  it('should create table element', () => {
+    expect(debug.queryAll(By.css('.table'))).toHaveSize(1);
+  });
+
+  it('should create thead element', () => {
+    expect(debug.queryAll(By.css('.table-dark'))).toHaveSize(1);
+  });
+
 });
--- src/app/services/vehicle.service.spec.ts
+++ src/app/services/vehicle.service.spec.ts
@@ -1,12 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
+import { Vehicle } from '../models/vehicle';
+import { faker } from '@faker-js/faker';
 import { VehicleService } from './vehicle.service';
+
+class HttpClientMock {
+  get = jasmine.createSpy('httpClient.get');
+}
 
 describe('VehicleService', () => {
   let service: VehicleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        VehicleService,
+        {
+          provide: HttpClient,
+          useClass: HttpClientMock
+        }
+      ]
+    });
     service = TestBed.inject(VehicleService);
   });
 
